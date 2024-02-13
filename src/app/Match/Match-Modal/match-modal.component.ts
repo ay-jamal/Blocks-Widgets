0a207c88e2406914d13a1584af87e738f8ea01db
Ayhem jamal <aylibyakiller@gmail.com>
2024-02-13 21:34:41 +0200

    creating the match component and modal

---
+++ src/app/Match/Match-Modal/match-modal.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-match-modal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './match-modal.component.html',
+  styleUrls: ['./match-modal.component.scss']
+})
+export class MatchModalComponent {
+
+}
---
+++ src/app/Match/match.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-match',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './match.component.html',
+  styleUrls: ['./match.component.scss']
+})
+export class MatchComponent {
+
+}
